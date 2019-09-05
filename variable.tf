c3a2ab518de81a3e8c1da48bb78e0231847bd0fa
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-09-05 18:25:36 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,18 @@
-
+variable "instance_type" {
+default = "n1-standard-1"
+}
+variable "zone_id" {
+default = "us-central1-a"
+}
+variable "image_name" {
+default = "ubuntu-os-cloud/ubuntu-1604-lts"
+}
+variable "disk_size" {
+default = 30
+}
+variable "region" {
+ default = "us-central1"
+}
+variable "project" {
+ default = "pure-spring-242015"
+}
