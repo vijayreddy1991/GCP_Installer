7c4ae3cbc4ec841b51a787185b1da443210e6ca9
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-09-05 18:24:46 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,28 @@
+provider "google" {
+  credentials = "${file("My First Project-4de6cc1605a7.json")}"
+  project     = "${var.project}"
+  region      = "${var.region}"
+}
 
+resource "google_compute_instance" "default" {
+  name         = "terraform-test"
+  machine_type = "${var.instance_type}"
+  zone         = "${var.zone_id}"
+
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image_name}"
+      size = "${var.disk_size}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+}
