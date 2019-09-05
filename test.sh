81967508db7ce63e48179ca7ac4f66f66a1cf3a9
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-09-05 12:03:12 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+echo "============ docker info ============"
+sudo docker info
+echo "============ list of docker containers ============"
+sudo docker ps 
+# sudo docker rm -f cron
+echo "============ checking nexec container is up ============"
+sudo docker ps | grep nexec
+echo "============ checking cron container is up============"
+sudo docker ps | grep cron
+echo "============ checking steptrigger containeris up ============"
+sudo docker ps | grep steptrigger
+echo "============ checking runtrigger container is up ============"
+sudo docker ps | grep runtrigger
+echo "============ checking pipelinesync container is up ============"
+sudo docker ps | grep pipelinesync
+echo "============ checking www container is up ============"
+sudo docker ps | grep www
+echo "============ checking api container is up ============"
+sudo docker ps | grep api
+echo "============ checking api response ============"
+curl localhost:30000
+echo ""
+echo "============ checking www response ============"
+curl localhost:30001
