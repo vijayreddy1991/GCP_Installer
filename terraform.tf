cd8f1a6c33d5be218ef76f5b5ac22ae392d796e6
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-09-06 09:46:45 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -26,6 +26,15 @@ resource "google_compute_instance" "default" {
   }
 
 }
+
+resource "google_compute_project_metadata" "default" {
+metadata = {
+   sshKeys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  }
+project = "${var.project}"
+
+
+
 output "ip" {
 value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
